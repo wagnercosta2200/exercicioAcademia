c28399c8e16d0def5d5046c4a50ebc51406058fd
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-02 22:14:49 -0300

    exercicio8

--- Exercicio6/Program.cs
+++ Exercicio6/Program.cs
@@ -2,25 +2,35 @@
 Console.WriteLine("6: Escreva um programa em C# e no Visual Studio que leia os dados\n"+" para o parafuso A e para o parafuso B,isto é, o código,\n"+" a quantidade de peças e o valor unitário de cada parafuso\n"+" e a porcentagem de IPI(única) a ser acrescentada. ");
 
 
-double k1;
-double k2;
-double Litro;
-double mediaLitro;
-double KmRodados;
-Console.WriteLine("Digite Km Inicial: ");
-k1 = double.Parse(Console.ReadLine());
+double ParafussoA;
+double ParafussoB;
+double quantidadePecas;
+double valorUniParafussoA;
+double valorUniParafussoB;
+double codigo;
+double porcentagem = ;
 
-Console.WriteLine("Digite Km final:");
-k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Leia Parafusso A: ");
+ParafussoA = double.Parse(Console.ReadLine());
 
+Console.WriteLine("Leia Parafusso B:");
+ParafussoB = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Digite quantidade de litros abastecidos");
-Litro = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor do Parafusso A: ");
+valorUniParafussoA = double.Parse(Console.ReadLine());
 
-KmRodados = k1 - k2;
+Console.WriteLine("Digite o valor do B: ");
+valorUniParafussoB = double.Parse(Console.ReadLine());
 
-mediaLitro = (k1 - k2) / Litro;
+quantidadePecas = ParafussoA + ParafussoB;
 
-Console.WriteLine("Rodamos km:" + KmRodados);
+Console.WriteLine("Quantidade de Peças: " + quantidadePecas);
 
-Console.WriteLine("A media de combustivel e´: " + mediaLitro);
+Console.WriteLine("Valor Unitário do Parafusso A: " + valorUniParafussoA);
+Console.WriteLine("Valor Unitário do Parafusso b: " + valorUniParafussoB);
+
+//mediaLitro = (k1 - k2) / Litro;
+
+//Console.WriteLine("Rodamos km:" + KmRodados);
+
+//Console.WriteLine("A media de combustivel e´: " + mediaLitro);
---
+++ Exercicio8/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("8: Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. \n " +"A fórmula da conversão é F = (9 * C + 160) / 5.");
+
+
+double C = 0;
+double f = 0; 
+
+Console.WriteLine("Digite a temperatura");
+C= double.Parse(Console.ReadLine());
+
+
+
+f = (9 * C + 160) / 5;
+
+Console.WriteLine("O valor da temperatura em Fahrenheit é : " + f);
+
