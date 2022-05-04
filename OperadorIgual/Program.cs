63d37ac9af8358c098a453ee454bd58ea87a991b
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-04 20:54:20 -0300

    Operador Igual ( != )

---
+++ OperadorIgual/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/// <summary>
+/// Oprerador ==
+/// </summary>
+
+
+int num1, num2;
+Console.WriteLine("Digite dois numeros: ");
+
+num1 = int.Parse(Console.ReadLine());
+num2 = int.Parse(Console.ReadLine());
+
+if (num1 == num2)
+{
+    Console.WriteLine("Os dois numeros são iguais");
+}
+
+if(num1 != num2)
+ {
+    Console.WriteLine("Os numeros são diferentes");
+}
+
+// Operador igual ( == )
+
+//if (num1 == num2)
+//{
+//    Console.WriteLine("Os dois numeros são iguais");
+//}
+
+//if (num1 != num2)
+//{
+//    Console.WriteLine("Os numeros são diferentes");
+//}
