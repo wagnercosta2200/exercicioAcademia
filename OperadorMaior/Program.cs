492804090644fde26ebc7a25e55a26c28eb7e946
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-04 20:43:28 -0300

    Operador Maior

---
+++ OperadorMaior/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int idade;
+Console.WriteLine("Digite a sua idade");
+idade = int.Parse(Console.ReadLine());
+if (idade> 17)
+    {
+    Console.WriteLine("Voce é maior de idade");
+}
