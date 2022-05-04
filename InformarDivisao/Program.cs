6b749268733e81c23441c152d4f419afa2c7162b
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-04 20:37:03 -0300

    Informar se o numero é divisivel por 2, 4 ou 8

---
+++ InformarDivisao/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+/// <summary>
+/// Informar se o numero é divisivel por 2, 4 ou 8
+/// </summary>
+
+int num;
+Console.WriteLine("Digite um numero: ");
+num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Divisivel por 2");
+}
+ if (num % 4 == 0)
+{
+    Console.WriteLine("Divisivel por 4");
+}
+ if (num % 8 == 0)
+    {
+    Console.WriteLine("Divisivel por 8");
+}
+
