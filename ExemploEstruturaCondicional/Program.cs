86f38cd1252319b5139bb68b05e66b0f0852c215
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-04 19:40:23 -0300

    Exemplo Estrutura Condicional

---
+++ ExemploEstruturaCondicional/Program.cs
@@ -0,0 +1,11 @@
+﻿static void Main (string[] args)
+{
+    int num;
+    Console.WriteLine("Digite um numero: ");
+    num = int.Parse(args[0]);
+
+    if (num > 0)
+    {
+        Console.WriteLine("Positivo");
+    }
+}
