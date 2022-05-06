1b0880a2083618a71311712de5022527adb81e25
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-06 18:47:31 -0300

    Exercicio2 Par ou Impar

---
+++ Exercico2_PAR_IMPAR/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("2 - Faça um algoritmo para ler um número e verificar se ele é par ou ímpar. ");
+
+int numero;
+
+Console.WriteLine("Digite um numero");
+numero = Convert.ToInt32(Console.ReadLine());
+
+/// <summary>
+/// Para verificar se é par ou impar
+/// </summary>
+
+if (numero % 2 == 0)
+    {
+    Console.WriteLine("Esse número é par");
+}
+    else
+{
+    Console.WriteLine("Esse número é impar");
+}
