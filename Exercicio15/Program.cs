51b478ecced1d7d00283e6a931723df3f7839c8e
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-05 21:27:02 -0300

    Exercicio15

---
+++ Exercicio15/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("15 - Faça um programa para uma loja de tintas.\n" + "O programa deverá pedir o tamanho em metros quadrados da área a ser pintada.\n" + "Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadradose que a tinta é vendida em latas de 18 litros, que custam R$ 80, 00.\n" +" Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.");
+
+/// <summary>
+/// Variaveis 
+/// </summary>
+float tamanhoArea;
+float quantidadeTinta;
+int quantidadeLatas;
+float valorCompras;
+
+Console.WriteLine("Informe a área a ser pintada (metros quadrados):");
+tamanhoArea = float.Parse(Console.ReadLine());
+
+quantidadeTinta = tamanhoArea / 3; // cobertura da tinta é de 1 litro por 3 metros quadrados
+quantidadeLatas =(int)Math.Ceiling(quantidadeTinta / 18);//tinta é vendida em latas de 18 litros
+valorCompras = quantidadeLatas * 80;//a lata custa R$ 80,00
+
+Console.WriteLine("A quantidade de tinta para cobrir a área é de :"+ quantidadeTinta + " Litros");
+Console.WriteLine("A quantidade de latas de 18 litros para cobrir a área é de :" + quantidadeLatas);
+Console.WriteLine("O valor total a ser gasto é de :" + valorCompras);
+{
+
+}
---
+++ ExercicioEstruturasCondicionais1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1 - Faça um algoritmo para calcular e mostrar a área de um triângulo \n " + "a partir dos valores da base e da altura \n " + "lidos pelo teclado: Área do triangulo = (base * altura) / 2; ");
+
+
