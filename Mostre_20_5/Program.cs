99f8a44eabf1d2e7c038a8d6e54a64772b40f672
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-11 20:42:53 -0300

    Escreva uma algoritmo que mostre o numeros de 20 a 5

---
+++ Mostre_20_5/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escreva uma algoritmo que mostre o numeros de 20 a 5");
+
+
+int i;
+
+for (i = 20; i >=5  ; i--)
+
+{
+    Console.WriteLine("I algortimo é  " + i);
+}
