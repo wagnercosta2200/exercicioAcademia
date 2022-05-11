482a18cdb6c0a9ea63703e366bd44ae8fd917ce0
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-11 16:04:47 -0300

    Exercicio16 Placar do jogo

---
+++ Exercicio16_Placar_Do_Jogo/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("16 - Escreva um algoritmo que leia o placar de um jogo da primeira fase da copa do Brasil. Caso o time de fora tenha ganho o jogo por mais de 2 gols de diferença, mostre \n " + " na tela uma mensagem indicando que o time de fora já se classificou para a próxima fase.\n " + "Caso contrário, mostre uma mensagem indicando que os dois times irão se enfrentar novamente em um novo jogo.\n" + "  ex: time da casa 4 x 3 time de fora, mostra Os dois times se enfrentarão em um novo jogo \n" +" ex2: time da casa 1 x 3 time de fora, mostra O time de fora já se classificou \n " + "plus: caso ocorra um segundo jogo, leia o placar desse novo jogo e então diga quem passou de fase.");
+
+int timeACasa, timeBFora, timeAFora = 0, timeBCasa = 0, penaltiTimeA = 0, penaltiTimeB = 0, difencaPrimeiroJogo;
+
+Console.Write("Informe o número de gols do time A: ");
+timeACasa = int.Parse(Console.ReadLine());
+Console.Write("Informe o número de gols do time B: ");
+timeBFora = int.Parse(Console.ReadLine());
+
+if ((timeBFora - timeACasa) >= 2)
+{
+    Console.WriteLine();
+    Console.WriteLine("O time B já se classificou.");
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine("Os dois times se enfrentarão em um novo jogo.");
+
+    Console.WriteLine();
+    Console.Write("Informe o número de gols do time B: ");
+    timeBCasa = int.Parse(Console.ReadLine());
+    Console.Write("Informe o número de gols do time A: ");
+    timeAFora = int.Parse(Console.ReadLine());
+
+    if (timeBCasa > timeAFora)
+    {
+        Console.WriteLine();
+        Console.WriteLine("O time B passou de fase.");
+    }
+    else if ((timeBCasa == timeAFora) && (timeAFora > timeBFora))
+    {
+        Console.WriteLine();
+        Console.WriteLine("O time A passou de fase.");
+    }
+    else if ((timeBCasa == timeAFora) && (timeBFora > timeAFora))
+    {
+        Console.WriteLine();
+        Console.WriteLine("O time B passou de fase.");
+    }
+    else if (timeBCasa == timeAFora)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Como os times A e B empataram, será necessário ir para os\n" +
+            "pênaltis, mas lembre-se de que não há empate nessa situação.");
+        Console.WriteLine();
+        Console.Write("Informe o número de pênaltis do time A: ");
+        penaltiTimeA = int.Parse(Console.ReadLine());
+        Console.Write("Informe o número de pênaltis do time B: ");
+        penaltiTimeB = int.Parse(Console.ReadLine());
+
+        if (penaltiTimeA > penaltiTimeB)
+        {
+            Console.WriteLine();
+            Console.WriteLine("O time A passou de fase.");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("O time B passou de fase.");
+        }
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("O time A já se classificou.");
+    }
+}
