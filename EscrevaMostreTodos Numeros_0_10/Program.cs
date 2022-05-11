e6b358aa489e653f7a23de31913584abcd54f42b
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-11 20:14:40 -0300

    Escreva um algoritmos que mostre todos os numeros de 0 a 10

---
+++ EscrevaMostreTodos Numeros_0_10/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escreva um algoritmos que mostre todos os numeros de 0 a 10");
+
+
+int i;
+
+for (i = 0; i <= 10; i++)
+{
+    Console.WriteLine("I algortimo é  " + i);
+}
