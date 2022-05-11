09e3b1a27959a9c119e29083fba1e52f7d14ae9f
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-11 20:58:54 -0300

    Escreva um algoritmo leia pelo teclado os dois valores

---
+++ LEIA_Pelo_Teclado/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escreva um algoritmo leia pelo teclado os dois valores \n " + " inteiros chamados inicio e fim, e então mostre todos os números entre inicio e fim");
+
+int i, inicio, fim;
+
+Console.WriteLine("Digite o inicio: ");
+inicio = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Digite o fim: ");
+fim = int.Parse(Console.ReadLine());
+
+if (inicio < fim)
+{
+    for (i = inicio; i <= fim; i++)
+    {
+        Console.WriteLine("i vale " + i);
+    }
+}
+else if (inicio > fim)
+{
+    for (i = inicio; i >= fim; i--)
+    {
+        Console.WriteLine("i vale " + i);
+    }
+}
+else
+{
+    Console.WriteLine("Os valores são iguais!:");
+}
