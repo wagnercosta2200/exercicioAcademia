2b12a270922ae270be1f9e6c3efbce3e65c1680d
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-11 16:11:32 -0300

    Exercicio 18 ler 3 valores e informar o maior

---
+++ Exercicio18/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("18 - Escreva um algoritmo que leia 3 valores pelo teclado e então informe qual o maior deles.");
+
+int valor1, valor2, valor3;
+
+Console.Write("Informe o primeiro valor: ");
+valor1 = int.Parse(Console.ReadLine());
+Console.Write("Informe um segundo valor: ");
+valor2 = int.Parse(Console.ReadLine());
+Console.Write("Informe um terceiro valor: ");
+valor3 = int.Parse(Console.ReadLine());
+
+if (valor1 > valor2 && valor1 > valor3)
+{
+    Console.WriteLine();
+    Console.WriteLine(valor1 + " é o maior valor.");
+}
+else if (valor2 > valor1 && valor2 > valor3)
+{
+    Console.WriteLine();
+    Console.WriteLine(valor2 + " é o maior valor.");
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine(valor3 + " é o maior valor.");
+}
