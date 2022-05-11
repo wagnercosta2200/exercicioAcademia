8b14177dedfac15a7b78e415f4f0912e9a24e751
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-11 16:48:39 -0300

    receba frases

---
+++ Exercicio7_Receba_Frase/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("7 - Faça um programa em C# e no VS para receber uma frase qualquer e uma palavra de pesquisa.\n " + "O programa deve avaliar se a palavra aparece na frase, informando o usuário via mensagem, como por\n " + "exemplo, A palavra encontra - se na frase   ou A palavra não se encontra na frase.");
+
+
+string frase;
+string palavraPesquisa;
+
+Console.Write("Digite uma frase: ");
+frase = Console.ReadLine();
+
+Console.Write("Digite uma palavra para procurar na frase: ");
+palavraPesquisa = Console.ReadLine();
+
+if (frase.Contains(palavraPesquisa) == true)
+{
+    Console.WriteLine();
+    Console.WriteLine("A palvra: " + palavraPesquisa + ", foi encontrada na frase informada.");
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine("A palvra: " + palavraPesquisa + ", não foi encontrada na frase informada.");
+}
