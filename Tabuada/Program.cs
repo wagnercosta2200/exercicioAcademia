a966909941c78697bbb1be591b600befec9f146e
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-11 21:58:42 -0300

    Exemplo tabuada

---
+++ Tabuada/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("tabuada");
+
+
+    int i, n,r;
+
+Console.WriteLine( "Digite a tabuada do numero que deseja apresentar : " );
+n = int.Parse( Console.ReadLine() );
+
+for ( i = 0; i <= 10; i++)
+{
+    //r = i * n;
+    //Console.WriteLine(i + " x " + n +" = " +r);
+    Console.WriteLine(i + " x " + n + " = " + i*n);
+}
+
