ababae9849f937777ac9da49dbd52abd3696bdca
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-12 20:04:20 -0300

    Nome de pessoas

---
+++ Exemplo_de_Repeticao_Letras/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Nome de pessoas");
+
+//Criação da lista Pessoas
+List<string> ListaPessoas = new List<string>();
+string nome;
+
+do
+{
+    Console.WriteLine("Digite um nome de pessoas ou a palavra sair pra encerrar o programa:");
+    nome = Console.ReadLine().ToUpper();
+  
+
+    if (nome == "sair")
+    {
+        break; // encerra a repetição
+    }
+    ListaPessoas.Add(nome);
+} while (true);
+
+//ListaPessoas = [0 ,1,2, 3, ....,n]
+
+for (int i =0; i < ListaPessoas.Count; i = i + 1)
+{
+    Console.WriteLine(ListaPessoas[i]);
+}
