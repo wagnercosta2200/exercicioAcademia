1cd9c52de8a79fb83d87ff3d88dde4c713136d88
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-11 21:23:32 -0300

    pulando de X em X

---
+++ EcrevaAlgoritmoPulando_X_X/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escreva um algoritmo leia pelo teclado um valor inteiro \n " + " chamado x, e então mostre na tela todos os números de 0 a 100, \n " + "pulando de x em x.");
+
+
+int i, x;
+
+Console.WriteLine("Digite x: ");
+x = int.Parse(Console.ReadLine());
+
+for ( i = 0; i <= 100; i = i + x)
+
+{
+    Console.WriteLine("i vale " +  i );
+
+}
