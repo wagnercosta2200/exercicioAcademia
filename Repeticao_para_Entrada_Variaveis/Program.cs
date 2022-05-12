38dec901bab0f347403de6d2b68793acf9a15f0d
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-12 19:44:34 -0300

    Repetição para validar entradas em variaveis

---
+++ Repeticao_para_Entrada_Variaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Repetição para validar entradas em variaveis");
+
+
+float notaBimestral;
+
+do {
+
+    Console.Clear();
+    Console.WriteLine("Digite uma nota válida [ 0 a 10];");
+    notaBimestral = float.Parse(Console.ReadLine());
+    
+    if (notaBimestral < 0 || notaBimestral > 10) 
+    {
+        Console.WriteLine("Nota invalida... presta atenção notas de 0 a 10!:");
+        Console.ReadKey();
+    }
+} while (notaBimestral < 0 || notaBimestral > 10);
+
+Console.WriteLine("Parabens... Voce digitou uma nota valida:");
