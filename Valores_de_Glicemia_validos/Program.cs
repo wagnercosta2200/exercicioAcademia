a0e3eece1c58e1db01d7d71076b43841b09db5a7
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-12 20:16:09 -0300

    Valores de Glicemia Validos

---
+++ Valores_de_Glicemia_validos/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Valores de Glicemia Validos");
+
+int valorGlicemia; //45 a 450
+List<int> ListaGlicemia = new List<int>();
+
+//Variavel maiuscula é uma constante (para ninguem alterar)
+int TOTAL = 5;
+for (int i = 0; i < TOTAL; i++) { 
+
+    do {
+        Console.WriteLine("Digite um valor de glicimia valido:");
+        valorGlicemia = int.Parse(Console.ReadLine());
+        if (valorGlicemia > 45 && valorGlicemia <= 450) {
+            break;
+
+        }else
+        {
+            Console.WriteLine("Valor invalido . Atente para 45 a 450!:");
+        }
+
+    } while (true);
+
+    ListaGlicemia.Add(valorGlicemia);
+}
+
+for (int i = 0; i < ListaGlicemia.Count; i = i + 1)
+{
+    Console.WriteLine(ListaGlicemia[i]);
+}
