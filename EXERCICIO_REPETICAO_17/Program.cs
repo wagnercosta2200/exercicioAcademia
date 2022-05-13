c4277971608b690fa8c4f2d1e80b30d8787a93fc
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-12 21:02:50 -0300

    Apresentar os quadrados dos números inteiros de 15 a 200

---
+++ EXERCICIO_REPETICAO_17/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Clear();   
+Console.WriteLine("17: Apresentar os quadrados dos números inteiros de 15 a 200.");
+
+int numero;
+
+for (numero = 15; numero <= 200; numero++){
+    Console.WriteLine("Quadrado de " + numero + " é "+ Math.Pow(numero,2));
+}
