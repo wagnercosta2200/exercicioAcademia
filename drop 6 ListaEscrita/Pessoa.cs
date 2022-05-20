db9a4daf9eed6cdaf20267fbb572ad6377dfa1ce
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-19 21:29:42 -0300

    DROP 6 LISTA ARQUIVO ESCRITA

---
+++ drop 6 ListaEscrita/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drop_6_ListaEscrita
+{
+    internal class Pessoa
+    {
+        string nome;
+        string email;
+
+        public Pessoa(string n , string e)
+        {
+            nome = n;
+            email = e;
+        }
+    }
+}
---
+++ drop 6 ListaEscrita/Pessoas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drop_6_ListaEscrita
+{
+    internal class Pessoas
+    {
+        string nome;
+        string email;
+
+        public Pessoas(string n, string e)
+        {
+            nome = n;
+            email = e;
+        }
+    }
+}
