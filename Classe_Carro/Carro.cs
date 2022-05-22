9efb0a714cd56230d92381b5f1b395ff5bb7ee16
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-22 17:56:53 -0300

    Classe Carro

---
+++ Classe_Carro/Carro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe_Carro
+{
+    internal class Carro
+    {
+        public string modelo ; 
+        public string marca  ;
+        public string cor  ; 
+        public string placa ;
+    }
+}
