2e27cf2b12bc4cf9aecddf84611009e63bf9f983
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-25 20:33:43 -0300

    Classe Livros

---
+++ Orientação a Objeto/Classe_LIVROS/Livro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseLivro
+{
+    public class Livro
+    {
+        //atributos
+        public String titulo { get => titulo; set => titulo = value; }
+        public String autor;
+        public int paginas;
+        public int ano;
+        public int edicao;
+        //construtor
+        public Livro(String titulo, String autor, int paginas, int ano, int edicao)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.paginas = paginas;
+            this.ano = ano;
+            this.edicao = edicao;
+        }
+        //metodos
+        public void apresentaInfoLivro()
+        {
+            Console.WriteLine("O livro com titulo de " + this.titulo + " do autor " + this.autor + " edição " + this.edicao + " lançado no ano de " + this.ano + " com " + this.paginas + " páginas");
+        }
+
+    }
+}
---
+++ Orientação a Objeto/Classe_LIVROS/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Classe Livros");
+
+
+using ClasseLivro;
+
+Console.WriteLine("Estamos no nosso projeto que trabalharemos com livros!");
+String t, a;
+int pag, ano, ed;
+Console.WriteLine("Digite o título do livro: ");
+t = Console.ReadLine();
+Console.WriteLine("Digite o autor do livro: ");
+a = Console.ReadLine();
+Console.WriteLine("Digite a quantidade de páginas: ");
+pag = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o ano de publicação do livro: ");
+ano = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a edição do livro: ");
+ed = int.Parse(Console.ReadLine());
+Livro l1 = new Livro(t, a, pag, ano, ed);
+l1.apresentaInfoLivro();
+
+
