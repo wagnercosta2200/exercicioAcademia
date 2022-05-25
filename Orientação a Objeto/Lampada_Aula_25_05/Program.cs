0f574491cdb10184b1a4ec77c158cec6c996f6d6
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-25 19:59:45 -0300

    Aula 25/05 Lampada

---
+++ Orientação a Objeto/Lampada_Aula_25_05/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Aula2505;
+int p1;
+String m1;
+Console.WriteLine("Olá mundo!!! Aqui, vamos cadastrar lampadas com o objetivo nenhum");
+Console.WriteLine("Digite a potencia da lampada: ");
+p1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a marca da lampada");
+m1 = Console.ReadLine();
+Lampada l = new Lampada(p1, m1);
+l.ligar();
+l.desligar();
+
+/*
+Console.WriteLine("A potencia da lampada é : "+l.potencia+" da marca: "+l.marca);
+Lampada l2 = new Lampada(4, "ELG");
+Console.WriteLine("A potencia da lampada é : " + l2.potencia + " da marca: " + l2.marca);
+Lampada l3 = new Lampada(10, "HAVIC");
+Console.WriteLine("A potencia da lampada é : " + l3.potencia + " da marca: " + l3.marca);
+l2.ligar();
+l2.desligar();
+l3.ligar();
+l3.desligar();
+*/
---
+++ Orientação a Objeto/Lampada_Aula_25_05/lampada.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2505
+{
+    public class Lampada
+    {
+        private bool ligado;
+        public int potencia;
+        public String marca;
+
+        public Lampada(int pot, String m)
+        {
+            potencia = pot;
+            marca = m;
+        }
+
+        public void ligar()
+        {
+            ligado = true;
+            Console.WriteLine("Ligando a lampada da marca: " + marca + " com potencia de: " + potencia);
+        }
+        public void desligar()
+        {
+            ligado = false;
+            Console.WriteLine("Desligando a lampada da marca: " + marca + " com potencia de: " + potencia);
+        }
+        public bool estaLigada()
+        {
+            return ligado;
+        }
+    }
+}
