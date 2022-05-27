e067fc843daed13fcfe440c5fc9e0e95eb9c58f0
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-27 16:22:42 -0300

    Garagem

--- Orientação a Objeto/Garagem/Veiculo.cs
+++ Orientação a Objeto/Garagem/Veiculo.cs
@@ -14,7 +14,7 @@ namespace Garagem
         private void gerarDataHoraEntrada()
         {
             this.datahoraEntrada = DateTime.Now;
-            //this.datahoraEntrada = this.datahoraEntrada.ToString();
+      
         }
 
         //construtor usado no cadastro de um usuario
