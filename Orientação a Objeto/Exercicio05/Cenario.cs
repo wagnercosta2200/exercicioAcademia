ce282f6af8e8a6886c1c913a24b32050b03d5555
Wagner Costa <wagnercosta2200@gmail.com>
2022-05-27 20:49:33 -0300

    Exercicio05

---
+++ Orientação a Objeto/Exercicio05/Cenario.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio05
+{
+    class Cenario
+    {
+        // guarda adescriçãpo cenário
+ 
+        string descricao;
+
+      
+        // guarda a data de criação do cenário
+      
+        DateTime data_criacao;
+
+        // guarda a altura do cenário
+       
+        float altura;
+
+   
+        // propriedade de acesso  da descrição
+       
+        public string Descricao { get => descricao; set => descricao = value; }
+
+        // propriedade de acesso da data de Criação
+       
+        public DateTime Data_criacao { get => data_criacao; set => data_criacao = value; }
+
+        /// <summary>
+        /// propriedade de acesso da altura
+        /// </summary>
+        public float Altura { get => altura; set => altura = value; }
+
+        // método construtor básico
+       
+        public Cenario()
+        {
+
+        }
+
+    
+        ////método construtor do cenário com todos atributos
+       
+        public Cenario(string descricao, DateTime data_criacao, float altura)
+        {
+            this.descricao = descricao;
+            this.data_criacao = data_criacao;
+            this.altura = altura;
+        }
+
+        // método que mostra os dados do cenário na tela do usuário
+  
+        public void exibirDados()
+        {
+            Console.WriteLine("Exibindo dados...");
+            Console.WriteLine(Descricao + "|" + Data_criacao.ToString("d") + "|" + Altura);
+        }
+
+      
+        // método que calcula o intervalo de tempo entre a data de criação e a data atual
+        
+        public TimeSpan calcularTempo()
+        {
+            //TimeSpan intervalo;~//não precis a de variável
+            //intervalo = DateTime.Now.Subtract(Data_criacao);
+            //intervalo = DateTime.Now - Data_criacao;
+            //return intervalo;
+            return DateTime.Now.Subtract(Data_criacao);
+           
+        }
+
+        public void imprimirCenario()
+        {
+            string arquivo = "teste.txt";
+            string caminho = @"C:\temp\";
+            string fullPath = System.IO.Path.Combine(caminho, arquivo);
+            Console.BackgroundColor = ConsoleColor.Green;
+            //StreamPrint
+            //printDocument1.DocumentName = arquivo;
+            //stringToPrint = System.IO.File.ReadAllText(fullPath);
+        }
+    }
+}
---
+++ Orientação a Objeto/Exercicio05/Program.cs
@@ -0,0 +1,35 @@
+﻿//#5 Exercicio05
+//Crie uma classe para representar um item de cenário, com os atributos privados de:
+//    *descrição(string), 
+//    *data de criação(date - https://docs.microsoft.com/en-us/dotnet/api/system.datetime.date?view=net-6.0) e
+//    *altura(float).
+
+//Crie os métodos públicos necessários para sets e gets, o construtor básico e também um método para imprimir 
+//todos dados de um item de cenário. 
+//Por fim, crie um método para calcular a quanto tempo o item foi criado, além de um programa em C# para testar
+//a classe criada.
+
+using Exercicio05;
+
+Cenario item = new Cenario();
+
+Console.WriteLine("Digite os dados de um cenário: ");
+
+Console.Write("Descrição: ");
+item.Descricao = Console.ReadLine();
+
+
+Console.Write("Data de Criação: ");
+item.Data_criacao = DateTime.Parse(Console.ReadLine());
+//item.Data_criacao = DateTime.Now;
+
+Console.Write("Altura: ");
+
+item.Altura = float.Parse(Console.ReadLine());
+
+item.exibirDados();
+
+//item.calcularTempo();
+Console.WriteLine("Tempo de criação: " + item.calcularTempo());
+Console.WriteLine("Data hoje Today: " + DateTime.Today + "Ano: " + DateTime.Today.Year);
+Console.WriteLine("Data Now(): " + DateTime.Now);
