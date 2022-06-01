8e160fdef098d62ee162c743812e96f5f2ea4252
Wagner Costa <wagnercosta2200@gmail.com>
2022-06-01 17:36:43 -0300

    Caso simples de Switch

---
+++ Switch/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Switch");
+
+/// <summary>
+/// Caso simple de Switch para encontrar o nome correto indicado na variavel.
+/// Switch utilizado para multiplas tomadas de decisão eliminando assim a utilização de muitos If/Else
+/// </summary>
+string nome = "Paulo";
+
+switch (nome)
+{
+    case "Joao": Console.WriteLine("Não é o cara");break;
+    case "Marcelo": Console.WriteLine("Não é o cara");break;
+    case "Paulo": Console.WriteLine("É este");break;
+    default: Console.WriteLine("Não encontrei"); break;
+}
