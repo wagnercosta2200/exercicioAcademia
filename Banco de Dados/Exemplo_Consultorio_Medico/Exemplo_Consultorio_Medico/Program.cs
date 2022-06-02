4bfb63f81cfae77eda0d6d04ce4df2e645cc8e56
Wagner Costa <wagnercosta2200@gmail.com>
2022-06-02 20:18:43 -0300

    Modelagem Consltorio Medico

---
+++ Banco de Dados/Exemplo_Consultorio_Medico/Exemplo_Consultorio_Medico/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Consultorio Medico");
+
+/* 
+    _ -------    MODELAGEM LÓGICA -------------
+    objetivo do sistema gerenciar  consultas e os CRUD
+    - Paciente (codigoPaciente, nome, ...)
+    - Medico (codigoMedico, nome, ...)
+    - EspecialidadeMedica (codigoEspecialidade, descrição)
+    - MedicoEspecialista (codigoMedico, codigoEspecialidade)
+    o codigoMedico é chave estrageira referencia Medico
+    o codigoEspecialidade é chave estrangeira referencia especialidadeMedica
+    - Convenio (codigoConvenio, nome)
+    - MedicoConvenioi (codigoMedico, codigoConvenio)
+*/
+public class Convenio
+{
+    int codigoConvenio;
+    string nome;
+}
+
+// construtores
+
+// get e setters
+public class Paciente
+{
+    int codigoPaciente;
+    string nome;
+}
+
+// construtores
+
+// get e setters
+
+public class Medico
+{
+    int codigoNome;
+    string nome;
+
+    public int CodigoNome { get => codigoNome; set => codigoNome = value; }
+    public string Nome { get => nome; set => nome = value; }
+}
+
+// construtores
+
+// get e setters
